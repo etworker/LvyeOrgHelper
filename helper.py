03af3b6468d87bdf36d69fe7387a3e09707e4cd5
etworker <etworker@outlook.com>
2016-05-30 19:44:07 +0800

    debug

--- helper.py
+++ helper.py
@@ -21,7 +21,7 @@ class LvyeOrgHelper(object):
         url = u"https://www.google.com/search?q=" + param
         # url = url.encode('utf-8')
         print url
-        return
+        # return
         html_cont = self.downloader.downloadPage(url.encode('utf-8'))
         filename = dir + "/" + self.lvye_id + ".html"
         print filename
